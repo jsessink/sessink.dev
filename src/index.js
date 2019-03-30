dbdeeb85bb84659ef761c7d025784593fd3145d3
Josh Sessink <jmsessink@gmail.com>
2019-03-29 21:53:58 -0500

    doin the node

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("YO!");
+	response.end("<img src='https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif' style='width: 100%;' />");
 });
 
 const port = process.env.PORT || 1337;
