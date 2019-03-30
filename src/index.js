8b67700f9096919fecabbd12a0fe887d06d3994b
Josh Sessink <jmsessink@gmail.com>
2019-03-29 22:05:13 -0500

    woops forgot to change the content type of the served file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("<img src='https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif' style='width: 100%;' />");
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<img src='https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif' style='width: 100%;' />");
+	response.end();
 });
 
 const port = process.env.PORT || 1337;
